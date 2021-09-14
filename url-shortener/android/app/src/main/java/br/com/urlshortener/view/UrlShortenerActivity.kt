36c6e136248254ee1b9193defb6af4dfeef9e8d1
Felipe Porge Xavier <felipe.porge@nubank.com.br>
2021-09-14 17:42:22 -0300

    Rename main activity

--- url-shortener/android/app/src/main/java/br/com/urlshortener/view/UrlShortenerActivity.kt
+++ url-shortener/android/app/src/main/java/br/com/urlshortener/view/UrlShortenerActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.urlshortener.R
 
-class MainActivity : AppCompatActivity() {
+class UrlShortenerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_url_shortener)
