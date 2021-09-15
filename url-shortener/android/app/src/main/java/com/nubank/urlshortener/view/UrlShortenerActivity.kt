70e57c7520c7c488354cb11899d2fbc6e7c8c507
Felipe Porge Xavier <felipefpx@gmail.com>
2021-09-15 10:03:32 -0300

    Updated android project (#9)
    * Recreate project
    
    * Add missing strings
    
    * Update xml and gradle
    
    * Rename main activity
    
    * Update project to make it easy for candidates
    
    * Add internet permission
    
    * Remove DS store and rename package
    
    * Rename package
    
    * Rename root project
    
    * Update url-shortener/android/app/build.gradle
    
    Co-authored-by: Lucas Lima <lucas.lima@nubank.com.br>
    
    * Update url-shortener/android/gradle.properties
    
    Co-authored-by: Lucas Lima <lucas.lima@nubank.com.br>
    
    * Reorganize the dependencies and replace jcenter with mavencentral
    
    Co-authored-by: Lucas Lima <lucas.lima@nubank.com.br>

--- url-shortener/android/app/src/main/java/com/nubank/urlshortener/view/UrlShortenerActivity.kt
+++ url-shortener/android/app/src/main/java/com/nubank/urlshortener/view/UrlShortenerActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import com.nubank.urlshortener.R
 
 class UrlShortenerActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_url_shortener)
